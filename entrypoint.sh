77ad5b9e3e828daf599206bb244c08f3127c7382
Pavel Alexeev <pahan@hubbitus.info>
2019-01-11 15:14:49 +0300

    Update to art-4.0 - Use variable for host: ${POSTGRES_HOST:-db}

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ psql -h db -U $POSTGRES_USER $POSTGRES_DB -c 'SELECT * FROM ART_DATABASE_VERSION
 		echo "Seams DB does not initialised! Try to do so"
 
 		set -e
-		sql="psql -qX1n -v ON_ERROR_STOP=1 -h db -U $POSTGRES_USER $POSTGRES_DB"
+		sql="psql -qX1n -v ON_ERROR_STOP=1 -h ${POSTGRES_HOST:-db} -U $POSTGRES_USER $POSTGRES_DB"
 
 		echo "Import main tables: art-tables.sql"
 		$sql < <( sed 's/ CLOB,/ TEXT,/g' /database/art-tables.sql )
